362a677d6fed60da2dc42d6e2cfaa8009b1d1022
Kian Edalat <akedalat@gmail.com>
2019-01-24 20:50:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,9 +4,9 @@ new_hash = {}
   languages.each do |style, language|
       language.each do |name, type|
         #binding.pry
-      new_hash[name] = type
+      new_hash[name] ||= type
 
-      new_hash[name][:style] ||= []
+      new_hash[name][:style] = []
       new_hash[name][:style] << style
       #binding.pry
     end
