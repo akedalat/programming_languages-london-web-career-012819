9a26b65884da6d933d0b38ee3f8a7762d30e4b4c
Kian Edalat <akedalat@gmail.com>
2019-01-24 20:43:45 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,12 +2,22 @@ require "pry"
 def reformat_languages(languages)
 new_hash = {}
 
+#languages.each do |oo_or_functional, language_hash|
+  #language_hash.each do |language, attribute_hash|
+    #attribute_hash.each do |attribute, str_value|
+      #if new_hash[language].nil?
+      #  new_hash[language] = {}
+      #end
+      #new_hash[language][:style] ||= []
+      #new_hash[language][:style] << oo_or_functional
+
   languages.each do |style, language|
       language.each do |name, type|
-        binding.pry
+        #binding.pry
+        
       new_hash[name] = type
 
-      new_hash[name][:style] = []
+      new_hash[name][:style] ||= []
       new_hash[name][:style] << style
       binding.pry
     end
